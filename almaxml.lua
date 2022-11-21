8259edbc66435b775d82c9227c2041ca02e0db19
Michal Hoftich <michal.h21@gmail.com>
2022-11-21 13:42:58 +0100

    use sax instead of dom

--- almaxml.lua
+++ almaxml.lua
@@ -1,84 +1,92 @@
 local domobject = require "luaxml-domobject"
+local xml_parser = require "luaxml-mod-xml"
 
-local M = {}
+-- mapping between child elements and names
+local mapping = {
+  C2 = "callno",
+  C16 = "sysno",
+  C17 = "author",
+  C18 = "title", 
+  C19 = "year"
+}
 
-local function get_colummn_names(dom)
-  -- get column names from XML schema which is included in ALMA XML
-  local mapping = {} 
-  local function find_xsd(root)
-    for i, child in ipairs(root:get_children()) do
-      -- xsd should be near the beginning
-      if i > 10 then return nil end
-      if child:is_element() then
-        if child:get_element_name() == "xsd:schema" then return child end
-        local xsd = find_xsd(child)
-        if xsd then return xsd end
-      end
-    end
-  end
-  local xsd = find_xsd(dom:root_node())
-  if not xsd then return nil, "Cannot find xsd" end
-  -- loop over xsd elements and get column name and it's title
-  for _, el in ipairs(xsd:query_selector("xsd|element")) do
-    local column = el:get_attribute("name")
-    local name = el:get_attribute("saw-sql:columnheading")
-    mapping[column] = name
-  end
-  return mapping
-end
-
-local function get_remap_table(mapping, remap)
-  -- map is C1 -> Alma name 
-  -- remap is Alma name -> newname
-  if not remap then return nil end
-  local newmap = {}
-  for column, name in pairs(mapping) do
-    local newname = remap[name]
-    if newname then
-      newmap[column] = newname
-    end
-  end
-  return newmap
-end
-
---- Process DOM and get table with records
--- mapping: mapping between XML record numbers and field names (from get_column_names function)
--- remap: table with field names that should be saved in the resulting table. Optional.
-local function process(dom, mapping, remap)
+local function load(filename)
+  -- DOM object is too slow  for this, see the newload function for what we use
+  local f, msg = io.open(filename, "r")
+  if not f then return nil, msg end
+  print "start"
+  local text =f:read("*all")
+  f:close()
+  print "xml read"
+  local dom = domobject.parse(text)
+  print "dom parsed"
   local records = {}
-  local remap_map = get_remap_table(mapping, remap)
-  for _, el in ipairs(dom:query_selector("R")) do
-    local rec = {}
-    for _, child in ipairs(el:get_children()) do
+  for _, rec in ipairs(dom:query_selector("R")) do
+    local current = {}
+    for _, child in ipairs(rec:get_children()) do
       if child:is_element() then
         local name = child:get_element_name()
-        if remap_map then 
-          local newname = remap_map[name]
-          if newname then
-            rec[newname] = child:get_text()
-          end
-        else
-          local newname = mapping[name]
-          rec[newname] = child:get_text()
+        local mapped_name = mapping[name]
+        if mapped_name then
+          current[mapped_name] = child:get_text()
         end
       end
     end
-    records[#records+1] = rec
+    local id = current.sysno
+    if id and not records[id] then
+      print(id, current.callno, current.author, current.title, current.year)
+      records[id] = current
+    end
   end
   return records
 end
 
-local function load(xmltext)
-  -- parse XML string to  DOM object
-  return domobject.parse(xmltext)
+
+-- @param filename -- ALMA XML file 
+-- @param fn -- callback function that will be executed on each individual record
+local function newload(filename, fn) 
+  local f, msg = io.open(filename, "r")
+  if not f then return nil, msg end
+  local text =f:read("*all")
+  f:close()
+  local records = {}
+  local current = {}
+  local current_name 
+  local handler = function()
+    local obj = {}
+    obj.starttag = function(self, name, attr)
+      local mapped_name = mapping[name]
+      if mapped_name then
+        current_name = mapped_name
+      else
+        current_name = nil
+      end
+    end
+    obj.endtag = function(self, name)
+      if name == "R" then
+        local id = current.sysno
+        if id and not records[id] then
+          -- print(id, current.callno, current.author, current.title, current.year)
+          records[id] = current
+          if fn then
+            fn(current)
+          end
+        end
+        current = {}
+      end
+    end
+    obj.text = function(self, text)
+      if current_name then
+        current[current_name] = text
+      end
+    end
+    return obj
+  end
+  local parser = xml_parser.xmlParser(handler())
+  parser:parse(text)
+  return records
 end
 
-
-
-
-M.load = load
-M.get_colummn_names = get_colummn_names
-M.get_remap_table = get_remap_table
-M.process = process
-
-return M
+return {
+  load = newload
+}
